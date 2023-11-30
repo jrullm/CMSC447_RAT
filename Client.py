39a38ecb7a76f39a005fb0080118cefce372ac4e
jrullm <104388636+jrullm@users.noreply.github.com>
2023-11-30 12:20:12 -0500

    Updated Client.py with initial code

--- Client.py
+++ Client.py
@@ -1,1 +1,14 @@
-#This is the client file By: Colby and Co.
+# echo-client.py
+# https://realpython.com/python-sockets/#tcp-sockets
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")#This is the client file By: Colby and Co.
