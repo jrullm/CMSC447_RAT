694edf24ed0b4a0c018c9d2cec5e890a835ed457
jrullm <104388636+jrullm@users.noreply.github.com>
2023-11-30 12:50:42 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -11,8 +11,19 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(MAX_SIZE)
-
-print(f"Received {data!r}")#This is the client file By: Colby and Co.
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        
+        with conn:
+            #Receiving data from client
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(MAX_SIZE)
+                if not data:
+                    break
+                pickle.loads(data).run()
+                
+            # Sending data to client
+            s.connect((HOST, PORT))
+            s.sendall(to_send)
