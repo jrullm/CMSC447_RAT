86827da2cbdd7f5ca2f47a3045b04c2dae692ae2
jrullm <104388636+jrullm@users.noreply.github.com>
2023-11-30 12:45:14 -0500

    Updated Client.py with maxsize passed

--- Client.py
+++ Client.py
@@ -2,6 +2,10 @@
 # https://realpython.com/python-sockets/#tcp-sockets
 
 import socket
+import pickle
+import sys
+
+MAX_SIZE = sys.maxsize
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
@@ -9,6 +13,6 @@ PORT = 65432  # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    data = s.recv(MAX_SIZE)
 
 print(f"Received {data!r}")#This is the client file By: Colby and Co.
