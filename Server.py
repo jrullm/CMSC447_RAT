b625fa3717cd7c66c691dec2cb0ad9d4114b4833
jrullm <104388636+jrullm@users.noreply.github.com>
2023-11-30 12:49:19 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -31,20 +31,24 @@ if __name__ == "__main__":
     
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-
+    data
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
+        
         with conn:
+            # Sending data to client
+            s.connect((HOST, PORT))
+            s.sendall(to_send)
+
+            #Receiving data from client
             print(f"Connected by {addr}")
             while True:
                 data = conn.recv(MAX_SIZE)
                 if not data:
                     break
-                conn.sendall(data)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(to_send)
-        data = s.recv(MAX_SIZE)
+                    
+            #Deserialize data
+            print(pickle.loads(data))
